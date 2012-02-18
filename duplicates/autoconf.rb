0c4484bb3605bfbf8ef434d3aec29a060e3b455f
reeze <reeze.xia@gmail.com>
2012-02-18 15:18:51 +0800

    Update duplicates/autoconf.rb

--- duplicates/autoconf.rb
+++ duplicates/autoconf.rb
@@ -5,13 +5,6 @@ class Autoconf < Formula
   homepage 'http://www.gnu.org/software/autoconf'
   md5 'c3b5247592ce694f7097873aa07d66fe'
 
-  keg_only <<-EOS.undent
-    The OSX shipped autoconf is more than 5 years old, and missing many
-    critical bug-fixes compared with the latest upstream.
-    In order to prevent conflicts with the system autoconf, this formula
-    is keg-only.
-  EOS
-
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make install"
